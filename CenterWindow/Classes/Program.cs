806fc917464c45d21dd01921b4de215cc210fad6
Karen Payne <paynekaren@comcast.net>
2022-07-17 05:48:48 -0700

    positions

---
+++ CenterWindow/Classes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using W = ConsoleHelperLibrary.Classes.WindowUtility;
+
+// ReSharper disable once CheckNamespace
+namespace CenterWindow
+{
+    partial class Program
+    {
+        [ModuleInitializer]
+        public static void Init()
+        {
+            Console.Title = "Code sample - centered";
+            W.SetConsoleWindowPosition(W.AnchorWindow.Center);
+        }
+    }
+}
+
+
+
+
+
---
+++ CenterWindow/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Spectre.Console;
+
+namespace CenterWindow
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            AnsiConsole.MarkupLine("[cyan]Press a key[/]");
+            Console.ReadLine();
+        }
+    }
+}
