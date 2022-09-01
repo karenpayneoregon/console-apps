4d5a1bac365b9a6254d4fe9a5c9556bda81ceaef
Karen Payne <paynekaren@comcast.net>
2022-09-01 10:52:01 -0700

    :arrow_right: format/comments

--- SpectreLibraryConsoleApp/Program.cs
+++ SpectreLibraryConsoleApp/Program.cs
@@ -7,31 +7,29 @@ namespace SpectreLibraryConsoleApp
     {
         static void Main(string[] args)
         {
-            //var password = Prompts.GetNewPassword("Enter a new password");
-            //Console.WriteLine(password);
+            var password = Prompts.GetNewPassword("Enter a new password");
+            Console.WriteLine(password);
 
 
-                                    TimeOnly? only = Prompts.GetTimeOnly($"{Now:HH}:00");
-                                    if (only is not null)
-                                    {
-                                        Console.WriteLine(only);
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("Null");
-                                    }
+            TimeOnly? only = Prompts.GetTimeOnly($"{Now:HH}:00");
+            if (only is not null)
+            {
+                Console.WriteLine(only);
+            }
+            else
+            {
+                Console.WriteLine("Null");
+            }
 
-                                    DateOnly? dateOnly = Prompts.GetDateOnly(Now.ToString("yyyy-MM-dd"));
-                                    if (dateOnly is not null)
-                                    {
-                                        Console.WriteLine(dateOnly.Value.ToString("MM/dd/yyyy"));
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("Null");
-                                    }
-
-
+            DateOnly? dateOnly = Prompts.GetDateOnly(Now.ToString("yyyy-MM-dd"));
+            if (dateOnly is not null)
+            {
+                Console.WriteLine(dateOnly.Value.ToString("MM/dd/yyyy"));
+            }
+            else
+            {
+                Console.WriteLine("Null");
+            }
 
 
             Console.ReadLine();
