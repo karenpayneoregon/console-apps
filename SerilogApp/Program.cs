2d15ecb35c6f213fa688d04428ac8866a48ef6dc
Karen Payne <paynekaren@comcast.net>
2023-01-19 18:02:53 -0800

    new project for serilog

---
+++ SerilogApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SpectreConsole;
+
+namespace SerilogApp;
+
+internal partial class Program
+{
+    static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.SpectreConsole("{Timestamp:HH:mm:ss} [{Level:u4}] {Message:lj}{NewLine}{Exception}", minLevel: LogEventLevel.Verbose)
+            .MinimumLevel.Verbose()
+            .CreateLogger();
+
+        // if appsettings.json is what you like...
+        //var configuration = new ConfigurationBuilder()
+        //    .SetBasePath(Directory.GetCurrentDirectory())
+        //    .AddJsonFile("appsettings.json")
+        //    .Build();
+
+        //Log.Logger = new LoggerConfiguration()
+        //    .ReadFrom.Configuration(configuration)
+        //    .CreateLogger();
+
+
+        Log.Verbose("Verbose level example with {0}", "parameter");
+        Log.Debug("Debug level example with {0}", "parameter");
+        Log.Information("Information level example with {0}", "parameter");
+        Log.Warning("Warning level example with {0}", "parameter");
+
+        try
+        {
+            File.ReadAllLines("NotHere");
+        }
+        catch (Exception exception)
+        {
+            Log.Error(exception, "Error level example with {0}", "parameter");
+        }
+
+ 
+        Console.ReadLine();
+    }
+}
---
+++ SerilogApp/readme.md
@@ -0,0 +1,11 @@
+﻿# About
+
+A third party NuGet package for `Serilog` using `Spectre.Console`. Note on when the exception is thrown, the path is not seen as per the following in the project file which is explained in [this repository](https://github.com/karenpayneoregon/hide-exception-paths).
+
+```xml
+<PropertyGroup>
+   <PathMap>$([System.IO.Path]::GetFullPath('$(MSBuildThisFileDirectory)'))=./</PathMap>
+</PropertyGroup>
+```
+
+![Screen](assets/screen.png)
