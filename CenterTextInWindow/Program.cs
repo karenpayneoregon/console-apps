ce033f4c92adbab7d3610d0f421322f22d7fbfdf
Karen Payne <paynekaren@comcast.net>
2023-01-31 10:09:14 -0800

    center window updated

--- CenterTextInWindow/Program.cs
+++ CenterTextInWindow/Program.cs
@@ -4,15 +4,33 @@ internal partial class Program
 {
     static void Main(string[] args)
     {
-        CenterLines("Hello world","Enjoy the ride");
+        ConsoleHelpers.CenterLinesFromTop("Hello world","Enjoy the ride");
         Console.ReadLine();
     }
 
+
+}
+
+public class ConsoleHelpers
+{
     public static void CenterLines(params string[] lines)
     {
 
         int verticalStart = (Console.WindowHeight - lines.Length) / 2;
         int verticalPosition = verticalStart;
+
+        foreach (var line in lines)
+        {
+            int horizontalStart = (Console.WindowWidth - line.Length) / 2;
+            Console.SetCursorPosition(horizontalStart, verticalPosition);
+            Console.Write(line);
+            ++verticalPosition;
+        }
+    }
+    public static void CenterLinesFromTop(params string[] lines)
+    {
+
+        int verticalPosition = 0;
 
         foreach (var line in lines)
         {
