4811c10ce9769ade92d6d9a0cd1e474d88d92ab5
Karen Payne <paynekaren@comcast.net>
2024-10-20 09:04:31 -0700

    clean-up

--- FullScreen/Classes/Program.cs
+++ FullScreen/Classes/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using W = ConsoleHelperLibrary.Classes.WindowUtility;
 
 // ReSharper disable once CheckNamespace
